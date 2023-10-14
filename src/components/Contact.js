0f92b4f58e79d7736a8005a3365d6811f306a0b5
Sri ram Koushik Batchu <sriram.koushikbatchu2021@vitstudent.ac.in>
2023-10-14 13:36:33 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="sriramkoushik143@gmail.com">Email</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/onboarding/start/follow-recommendations/new/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/dashboard" target="_blank" rel="noopener noreferrer">github</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
